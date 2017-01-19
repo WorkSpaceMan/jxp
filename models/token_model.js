cf0977ae51f5a9b716757cec75ad27e6d6971883
Jason Norwood-Young <jason@10layer.com>
2017-01-19 16:44:47 +0200

    Objectid => ObjectId

--- models/token_model.js
+++ models/token_model.js
@@ -1,16 +1,16 @@
 var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
 
-var Objectid = mongoose.Schema.Types.ObjectId;
+var ObjectId = mongoose.Schema.Types.ObjectId;
 
 var TokenSchema   = new Schema({
-	user_id: { type: Objectid, index: true },
+	user_id: { type: ObjectId, index: true },
 	provider: String,
 	access_token: String,
 	token_type: String,
 	expires_in: Number,
 	created: { type: Date, default: Date.now },
-	_owner_id: Objectid,
+	_owner_id: ObjectId,
 });
 
 TokenSchema.set("_perms", {
--- models/usergroups_model.js
+++ models/usergroups_model.js
@@ -1,10 +1,10 @@
 var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
 
-var Objectid = mongoose.Schema.Types.ObjectId;
+var ObjectId = mongoose.Schema.Types.ObjectId;
 
 var UserGroupSchema   = new Schema({
-	user_id: Objectid,
+	user_id: ObjectId,
 	groups: [String],
 	_date: { type: Date, default: Date.now },
 });
