2649a97785eeb6c5c7377e2aabda543dda9de75d
Jason Norwood-Young <jason@10layer.com>
2018-04-20 09:05:43 +0200

    Fix problem with missing user in call

--- libs/jexpress.js
+++ libs/jexpress.js
@@ -360,6 +360,7 @@ var actionDelete = function(req, res) {
 
 var actionCall = function(req, res) {
 	// console.log({ action_id: 7, action: "Method called", type: req.modelname, method: req.params.method_name, user: filterLogUser(req.user) });
+	req.body = req.body || {};
 	req.body.__user = req.user || null;
 	req.Model[req.params.method_name](req.body).then(
 		function(result) {
