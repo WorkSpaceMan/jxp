5d6887a6d216e636987d3ee79406511b19f0b9f3
Jason Norwood-Young <jason@10layer.com>
2019-03-11 13:34:50 +0100

    Stop the no-cache freakout

--- libs/cache.js
+++ libs/cache.js
@@ -1,7 +1,4 @@
 const config = require("config");
-const crypto = require('crypto');
-
-console.log("Cache", config.memcached);
 
 if (!config.memcached) {
     class Cache {
@@ -15,77 +12,78 @@ if (!config.memcached) {
             next();
         }
     }
-    return module.exports = Cache;
-}
+    module.exports = Cache;
+} else {
+    console.log("Enabling Cache", config.memcached);
+    const crypto = require('crypto');
+    const Memcached = require("memcached");
+    const memcached = new Memcached(config.memcached.server);
 
-const Memcached = require("memcached");
-const memcached = new Memcached(config.memcached.server);
-
-
-class Cache {
-    constructor() {
-    }
-
-    async read(req, res, next) {
-        const key = this.generateKey(req);
-        try {
-            const data = await this.mget(key);
-            if (data)
-                return res.send(data);
-
-            const oldSend = res.send;
-            res.send = function(data) {
-                oldSend.apply(this, arguments);
-            }
-
-            return next();
-        } catch(err) {
-            console.error(err);
-            res.send(500, err);
+    class Cache {
+        constructor() {
         }
-    }
 
-    async flush(req, res, next) {
-        memcached.flush(err => {
-            if (err)
+        async read(req, res, next) {
+            const key = this.generateKey(req);
+            try {
+                const data = await this.mget(key);
+                if (data)
+                    return res.send(data);
+
+                const oldSend = res.send;
+                res.send = function() {
+                    oldSend.apply(this, arguments);
+                }
+
+                return next();
+            } catch(err) {
                 console.error(err);
-            next();
-        });
-    }
-
-    generateKey(req) {
-        var params = [];
-        for (let i in params) {
-            params.push(i + "=" + params[i]);
+                res.send(500, err);
+            }
         }
-        params.sort();
-        const str = `${ req.url }?${ params.join("&") }`;
-        return crypto.createHash('md5').update(str).digest("hex");
+
+        async flush(req, res, next) {
+            memcached.flush(err => {
+                if (err)
+                    console.error(err);
+                next();
+            });
+        }
+
+        generateKey(req) {
+            var params = [];
+            for (let i in params) {
+                params.push(i + "=" + params[i]);
+            }
+            params.sort();
+            const str = `${ req.url }?${ params.join("&") }`;
+            return crypto.createHash('md5').update(str).digest("hex");
+        }
+
+        mget(key) {
+            return new Promise((resolve, reject) => {
+                memcached.get(key, (err, data) => {
+                    if (err)
+                        return reject(err);
+                    if (data)
+                        return resolve(data);
+                    return resolve(null);
+                })
+            });
+        }
+
+        msave(key, data) {
+            return new Promise((resolve, reject) => {
+                memcached.set(key, data, config.memcached.lifetime, (err) => {
+                    if (err)
+                        return reject(err);
+                    if (data)
+                        return resolve(data);
+                    return resolve(null);
+                })
+            });
+        }
     }
 
-    mget(key) {
-        return new Promise((resolve, reject) => {
-            memcached.get(key, (err, data) => {
-                if (err)
-                    return reject(err);
-                if (data)
-                    return resolve(data);
-                return resolve(null);
-            })
-        });
-    }
-
-    msave(key, data) {
-        return new Promise((resolve, reject) => {
-            memcached.set(key, data, config.memcached.lifetime, (err) => {
-                if (err)
-                    return reject(err);
-                if (data)
-                    return resolve(data);
-                return resolve(null);
-            })
-        });
-    }
+    module.exports = Cache;
 }
-
-module.exports = Cache;
