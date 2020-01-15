ce2a9257249372b4bed48e7d1bd312e332d879dc
Jason Norwood-Young <jason@10layer.com>
2020-01-15 13:48:01 +0100

    Refactor groups to an async/await pattern

--- libs/groups.js
+++ libs/groups.js
@@ -1,24 +1,25 @@
 var Groups = null;
 
-var init = function(config) {
+var init = config => {
 	var path = require("path");
 	Groups = require(path.join(config.model_dir, 'usergroups_model'));
 };
 
-var actionPut = function(req, res, next) {
-	var user_id = req.params.user_id;
-	var group = req.body.group;
+var actionPut = async (req, res) => {
+	const user_id = req.params.user_id;
+	const group = req.body.group;
 	if (!group) {
 		console.error("Group required");
 		res.send(400, "Group required");
 		return;
 	}
-	Groups.findOne({ user_id: user_id }, function(err, userGroup) {
-		if (err) {
-			console.error(err);
-			res.send(500, err);
-			return;
-		}
+	if (!user_id) {
+		console.error("user_id required");
+		res.send(400, "user_id required");
+		return;
+	}
+	try {
+		let userGroup = await Groups.findOne({ user_id: user_id });
 		if (!userGroup) {
 			userGroup = new Groups();
 			userGroup.user_id = user_id;
@@ -35,75 +36,88 @@ var actionPut = function(req, res, next) {
 				userGroup.groups.push(group);
 			}
 		}
-		userGroup.save(function(err, result) {
-			if (err) {
-				console.error(err);
-				res.send(500, err);
-				return;
-			}
-			res.send(result);
-		});
-	});
-};
-
-var actionPost = function(req, res, next) {
-	var user_id = req.params.user_id;
-	var group = req.body.group;
-	if (!group) {
-		group = [];
-	}
-	Groups.findOne({ user_id: user_id }, function(err, userGroup) {
-		if (err) {
+		res.send(await userGroup.save());
+	} catch(err) {
+			console.error(err);
 			res.send(500, err);
 			return;
-		}
+	}
+};
+
+var actionPost = async (req, res) => {
+	const user_id = req.params.user_id;
+	const group = req.body.group;
+	if (!group) {
+		console.error("Group required");
+		res.send(400, "Group required");
+		return;
+	}
+	if (!user_id) {
+		console.error("user_id required");
+		res.send(400, "user_id required");
+		return;
+	}
+	try {
+		let userGroup = await Groups.findOne({ user_id: user_id });
 		if (!userGroup) {
 			userGroup = new Groups();
 			userGroup.user_id = user_id;
 		}
 		userGroup.groups = [];
 		if (Array.isArray(group)) {
-			userGroup.groups = group;
+			group.forEach(function (g) {
+				if (userGroup.groups.indexOf(g) == -1) {
+					userGroup.groups.push(g);
+				}
+			});
 		} else {
-			userGroup.groups.push(group);
-		}
-		userGroup.save(function(err, result) {
-			if (err) {
-				res.send(500, err);
-				return;
+			if (userGroup.groups.indexOf(group) == -1) {
+				userGroup.groups.push(group);
 			}
-			res.send(result);
-		});
-	});
+		}
+		res.send(await userGroup.save());
+	} catch (err) {
+		console.error(err);
+		res.send(500, err);
+		return;
+	}
 };
 
-var actionGet = function(req, res, next) {
+var actionGet = async (req, res) => {
 	var user_id = req.params.user_id;
-	Groups.findOne({ user_id: user_id }, function(err, userGroup) {
-		if (err) {
-			res.send(500, err);
-			return;
-		}
+	if (!user_id) {
+		console.error("user_id required");
+		res.send(400, "user_id required");
+		return;
+	}
+	try {
+		let userGroup = await Groups.findOne({ user_id });
 		if (!userGroup) {
 			res.send({ groups: [] });
 			return;
 		}
 		res.send(userGroup);
-	});
+	} catch (err) {
+		console.error(err);
+		res.send(500, err);
+		return;
+	}
 };
 
-var actionDelete = function(req, res, next) {
-	var user_id = req.params.user_id;
-	var group = req.query.group;
+var actionDelete = async (req, res) => {
+	const user_id = req.params.user_id;
+	const group = req.query.group;
 	if (!group) {
 		res.send(400, "Group required");
 		return;
 	}
-	Groups.findOne({ user_id: user_id }, function(err, userGroup) {
-		if (err) {
-			res.send(500, err);
-			return;
-		}
+	if (!user_id) {
+		console.error("user_id required");
+		res.send(400, "user_id required");
+		return;
+	}
+	try {
+		let userGroup = await Groups.findOne({ user_id });
 		if (!userGroup) {
 			res.send(400, "User not found");
 			return;
@@ -112,14 +126,12 @@ var actionDelete = function(req, res, next) {
 		if (i > -1) {
 			userGroup.groups.splice(i, 1);
 		}
-		userGroup.save(function(err, result) {
-			if (err) {
-				res.send(500, err);
-				return;
-			}
-			res.send(result);
-		});
-	});
+		res.send(await userGroup.save());
+	} catch (err) {
+		console.error(err);
+		res.send(500, err);
+		return;
+	}
 };
 
 module.exports = {
