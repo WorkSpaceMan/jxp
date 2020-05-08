76877dfb8d616dcc8a7dc311900dd386dff2c61b
Jason Norwood-Young <jason@10layer.com>
2020-05-08 06:29:17 +0200

    Convert to new type

--- models/link_model.js
+++ models/link_model.js
@@ -1,19 +1,20 @@
-const mongoose     = require('mongoose');
-const Schema       = mongoose.Schema;
-const ObjectId     = mongoose.Schema.Types.ObjectId;
+/* global ObjectId */
+
+const Schema     = require("../libs/schema");
 
 const LinkSchema   = new Schema({
     name: String,
     val: String,
     _owner_id: ObjectId
+},
+{
+    perms: {
+        admin: "crud",
+        owner: "crud",
+        user: "cr",
+        all: "r"
+    }
 });
 
-// We can set permissions for different user types and user groups
-LinkSchema.set("_perms", {
-    admin: "crud", // CRUD = Create, Retrieve, Update and Delete
-    owner: "crud",
-    user: "cr",
-    all: "r" // Unauthenticated users will be able to read from test, but that is all
-});
-
-module.exports = mongoose.model('Link', LinkSchema);
+const Link = Schema.model('Link', LinkSchema);
+module.exports = Link;
