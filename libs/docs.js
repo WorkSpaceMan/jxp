e5643cbbc1eeca4a90f4c92cd010964f9b7f8494
Jason Norwood-Young <jason@10layer.com>
2021-06-14 15:23:59 +0200

    Move "schema_description" to its own file

--- libs/docs.js
+++ libs/docs.js
@@ -5,6 +5,7 @@ const yaml = require('js-yaml');
 const md = require('jstransformer')(require('jstransformer-markdown-it'));
 const util = require('util');
 const readFile = util.promisify(fs.readFile);
+const schema_description = require("./schema_description");
 
 class Docs {
     constructor(opts) {
@@ -37,7 +38,7 @@ class Docs {
 
     async metaModels(req, res) {
         try {
-            const models = await getModelFileContents(this.config.model_dir);
+            const models = await schema_description(this.config.model_dir);
             res.send(models);
         } catch (err) {
             console.error(err);
@@ -95,45 +96,6 @@ class Docs {
             res.send(500, { status: "error", error: err, message: err.toString() });
         }
     }
-}
-
-const getModelFileContents = (model_dir) => {
-    return new Promise((resolve, reject) => {
-        try {
-            fs.readdir(model_dir, function(err, files) {
-                if (err) {
-                    return reject(err);
-                }
-                let models = [];
-                for (file of files) {
-                    const modelname = path.basename(file, ".js").replace("_model", "");
-                    try {
-                        const modelobj = require(path.join(model_dir, file));
-                        if (
-                            modelobj.schema &&
-                            modelobj.schema.get("_perms") &&
-                            (modelobj.schema.get("_perms").admin ||
-                                modelobj.schema.get("_perms").user ||
-                                modelobj.schema.get("_perms").owner ||
-                                modelobj.schema.get("_perms").all)
-                        ) {
-                            let model = {
-                                model: modelname,
-                                file: file,
-                                perms: modelobj.schema.get("_perms")
-                            };
-                            models.push(model);
-                        }
-                    } catch (error) {
-                        console.error("Error with model " + modelname, error);
-                    }
-                }
-                return resolve(models);
-            });
-        } catch(err) {
-            return reject(err);
-        }
-    });
 }
 
 module.exports = Docs;
---
+++ libs/schema_description.js
@@ -0,0 +1,43 @@
+const fs = require("fs");
+const path = require("path");
+
+const getModelFileContents = (model_dir) => {
+    return new Promise((resolve, reject) => {
+        try {
+            fs.readdir(model_dir, function(err, files) {
+                if (err) {
+                    return reject(err);
+                }
+                let models = [];
+                for (file of files) {
+                    const modelname = path.basename(file, ".js").replace("_model", "");
+                    try {
+                        const modelobj = require(path.join(model_dir, file));
+                        if (
+                            modelobj.schema &&
+                            modelobj.schema.get("_perms") &&
+                            (modelobj.schema.get("_perms").admin ||
+                                modelobj.schema.get("_perms").user ||
+                                modelobj.schema.get("_perms").owner ||
+                                modelobj.schema.get("_perms").all)
+                        ) {
+                            let model = {
+                                model: modelname,
+                                file: file,
+                                perms: modelobj.schema.get("_perms")
+                            };
+                            models.push(model);
+                        }
+                    } catch (error) {
+                        console.error("Error with model " + modelname, error);
+                    }
+                }
+                return resolve(models);
+            });
+        } catch(err) {
+            return reject(err);
+        }
+    });
+}
+
+module.exports = getModelFileContents;
